0726b2acf373212efde4d3b3a03e32a941f41af1
Joaquin Descotte <157441085+Joaco273@users.noreply.github.com>
2026-08-26 17:09:20 -0400

    Add: is-anagram - submission-1

---
+++ Data Structures & Algorithms/is-anagram/submission-1.py
@@ -0,0 +1,20 @@
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+        if len(s) != len(t):
+            return False
+        freqA = {}
+        freqB = {}
+
+        for letter in s:
+            if (letter in freqA):
+                freqA[letter] += 1
+            else:
+                freqA[letter] = 1
+
+        for letter in t:
+            if (letter in freqB):
+                freqB[letter] += 1
+            else:
+                freqB[letter] = 1
+
+        return freqA == freqB
